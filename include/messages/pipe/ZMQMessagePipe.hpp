2d72fad14e3536a977e1340d4a67ef3f18330534
Theo Lincke <thli2739@colorado.edu>
2019-09-12 11:33:16 -0600

    second commit

--- include/messages/pipe/ZMQMessagePipe.hpp
+++ include/messages/pipe/ZMQMessagePipe.hpp
@@ -29,21 +29,13 @@ class ZMQPipe {
 
     bool set_filter(const std::string& id, std::unique_ptr<FilterInterface> filter) override;
 
-    /**
-     * @brief Does a two way signal handler, ideally the child process
-     * will send a signal back that means it has cleanly exited
-     * 
-     * @param id The id of the message pipe to kill
-     *
-     * @return 
-     */
     bool kill_cleanly(const std::string& id);
     void kill(const std::string& id);
 
   private:
   
-    typedef std::function<bool(std::unique_ptr<FilterInterface>)> filter_setter;
-    typedef std::function<bool(const int32_t)> signal_setter;
+    typedef std::function<int(std::unique_ptr<FilterInterface>)> filter_setter;
+    typedef std::function<int(const int32_t)> signal_setter;
     
     struct process_function_table {
       int32_t pid;
---
+++ include/messages/pipe/ZMQServerProxy.hpp
@@ -0,0 +1,33 @@
+/**
+ * @author      : theo (theo@varnsen)
+ * @file        : ZMQServerProxy
+ * @brief 
+ *
+ *
+ *
+ * @created     : Thursday Sep 12, 2019 10:29:34 MDT
+ * @bugs No Known Bugs
+ */
+
+#ifndef ZMQSERVERPROXY_HPP
+
+#define ZMQSERVERPROXY_HPP
+
+// C++ Includes
+
+// Local Includes
+#include "core/messages/pipe/FilterInterface.hpp"
+
+class ZMQServerProxy {
+  public:
+    ZMQServerProxy() {}
+    ~ZMQServerProxy() {}
+
+
+
+}
+
+
+
+#endif /* end of include guard ZMQSERVERPROXY_HPP */
+
