28a2a30f2e922dab88f8f704dee340e4fcc44c54
tlincke125 <thli2739@colorado.edu>
2019-10-13 22:06:13 -0600

    formated

---
+++ include/kernel/STDOutKermitOI.hpp
@@ -0,0 +1,23 @@
+/**
+ * @author      : theo (theo@$HOSTNAME)
+ * @file        : STDOutKermitOI
+ * @created     : Sunday Oct 13, 2019 22:05:39 MDT
+ */
+
+#ifndef STDOUTKERMITOI_HPP
+
+#define STDOUTKERMITOI_HPP
+
+#include "kernel/KermitOutputInterface.hpp"
+
+class STDOutKermitOI : public
+{
+public:
+  STDOutKermitOI();
+  virtual ~STDOutKermitOI();
+
+private:
+  /* private data */
+};
+
+#endif /* end of include guard STDOUTKERMITOI_HPP */
