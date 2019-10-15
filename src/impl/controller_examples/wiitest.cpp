d98e11aca96dcd32d14a1c6b5fae554a8fe8d0d7
PaperXLV <samuel.j.williams@colorado.edu>
2019-10-14 22:49:56 -0600

    wiitest changes to use kermit funcitons

--- src/impl/controller_examples/wiitest.cpp
+++ src/impl/controller_examples/wiitest.cpp
@@ -5,24 +5,20 @@
 #include <chrono>
 
 using namespace std;
-using namespace scpp::curmt;
 
 int main(int argc, char* argv[]) 
 {
-    Wiimote wii;
-    int error = wii.connect();
-    if (!error) 
+    scpp::curmt::Wiimote wii;
+    wii.start_kermit("aaaa");
+    if (wii.heartbeat()) 
     {
         cout << "Beginning Polling. No exit for now so ctrl-c ;)" << endl << "B to rumble, A to stop\n";
-        while(wii.heartbeat()) 
-        {
-            wii.loop(chrono::high_resolution_clock::now());
-        }
+        wii.loop_kermit();
     }
     else 
     {
         cout << "Unable to connect" << endl;
     }
-
+    wii.quit_kermit();
     return 0;
 }
