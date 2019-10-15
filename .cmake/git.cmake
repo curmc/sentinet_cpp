a09685c3aaa0241d6b0000502e4f2f956a7e7a8c
tlincke125 <thli2739@colorado.edu>
2019-10-15 14:28:48 -0600

    added proxy

--- .cmake/git.cmake
+++
@@ -1,7 +0,0 @@
-find_program(
-  GIT_EXECUTABLE
-  NAMES git
-  DOC "git command line client"
-  )
-mark_as_advanced(GIT_EXECUTABLE)
-
--- main.cpp
+++ main.cpp
@@ -5,27 +5,26 @@
 #include <string>
 
 int main() {
-
   auto a = std::make_unique<scpp::net::ZMQControlClient>();
-
   std::string value_to_publish = "Hello";
-
   a->publish(
       "tcp://localhost:5570",
-
       "cmd_vel",
-
       [&]() -> std::string & {
         std::cout << "Sending " << value_to_publish << std::endl;
-
         return value_to_publish;
       },
-
       std::chrono::seconds(1));
 
-  sleep(3);
+  a->subscribe(
+      "tcp://localhost:5571",
+      "cmd_vel",
+      [&](const std::string& value) -> void {
+        std::cout<<"                    Recieved: "<<value <<std::endl;
+        });
 
+
+  sleep(7);
   a->quit();
-
   return 0;
 }
