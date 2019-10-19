58f02e664b6de794dc573067312e6c5e5ad19399
tlincke125 <thli2739@colorado.edu>
2019-10-18 20:37:53 -0600

    added template class

---
+++ include/scpp/messages/pipe/TransformationFilter.hpp
@@ -0,0 +1,50 @@
+/**
+ * @author      : theo (theo.j.lincke@gmail.com)
+ * @file        : TransformationFilter
+ * @brief       : A brief description of the file
+ *
+ * A detailed description of the file
+ *
+ * @created     : Friday Oct 18, 2019 20:34:07 MDT
+ * @license     : MIT
+ */
+
+#ifndef TRANSFORMATIONFILTER_HPP
+
+#define TRANSFORMATIONFILTER_HPP
+
+// C++ includes
+#include <cmath>
+
+// Local includes
+#include "scpp/core/messages/pipe/FilterInterface.hpp"
+
+
+struct clean_message {
+  uint8_t possitionx;
+  uint8_t possitiony;
+  float depth;
+};
+
+struct raw_data {
+  // ???  Sensor values?
+  int distance_sensed;
+};
+
+class TransformationFilter : public ::scpp::core::FilterInterface
+{
+  public:
+    TransformationFilter (){};
+    virtual ~TransformationFilter () = default;
+
+    void convert(std::string& incomming_message) override;
+
+  private:
+    
+    clean_message transform(raw_data& data);
+
+    
+};
+
+#endif /* end of include guard TRANSFORMATIONFILTER_HPP */
+
---
+++ src/messages/pipe/TransformationFilter.cpp
@@ -0,0 +1,9 @@
+/**
+ * @author      : theo (theo.j.lincke@gmail.com)
+ * @file        : TransformationFilter
+ * @created     : Friday Oct 18, 2019 20:34:09 MDT
+ */
+
+#include "TransformationFilter.h"
+
+clean_message TransformationFilter::transform(raw_data& data);
