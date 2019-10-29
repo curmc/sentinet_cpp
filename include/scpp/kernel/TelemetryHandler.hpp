c15827a96219b064376d310da4771a83b7fd3106
tlincke125 <thli2739@colorado.edu>
2019-10-29 17:27:16 -0600

    set up platform for encrypted curl

---
+++ include/scpp/kernel/TelemetryHandler.hpp
@@ -0,0 +1,34 @@
+/**
+ * @author      : theo (theo.j.lincke@gmail.com)
+ * @file        : TelemetryHandler
+ * @brief       : A brief description of the file
+ *
+ * A detailed description of the file
+ *
+ * @created     : Tuesday Oct 29, 2019 17:23:28 MDT
+ * @license     : MIT
+ */
+
+#ifndef TELEMETRYHANDLER_HPP
+
+#define TELEMETRYHANDLER_HPP
+
+#include <curl/curl.h>
+#include <stdio.h>
+
+class TelemetryHandler
+{
+  public:
+    TelemetryHandler ();
+    virtual ~TelemetryHandler ();
+  private:
+    CURL* curl;
+    CURLcode res;
+
+  typedef struct {
+    
+  }
+};
+
+#endif /* end of include guard TELEMETRYHANDLER_HPP */
+
