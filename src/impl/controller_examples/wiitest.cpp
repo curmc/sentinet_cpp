0c4efbb13ce005dab04cd461e5a336fb6680c289
tlincke125 <thli2739@colorado.edu>
2019-10-31 18:45:19 -0600

    ads

---
+++ src/impl/controller_examples/wiitest.cpp
@@ -0,0 +1,22 @@
+#include "wiimote.h"
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+
+int main(int argc, char* argv[]) 
+{
+    scpp::curmt::Wiimote wii;
+    wii.start_kermit();
+    if (wii.heartbeat()) 
+    {
+        cout << "Beginning Polling. No exit for now so ctrl-c ;)" << endl << "B to rumble, A to stop\n";
+        wii.loop_kermit();
+    }
+    else 
+    {
+        cout << "Unable to connect" << endl;
+    }
+    wii.quit_kermit();
+    return 0;
+}
